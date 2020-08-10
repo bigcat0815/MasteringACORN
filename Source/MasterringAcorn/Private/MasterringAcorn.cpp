a4d2c6953c6930eb9979c09a4678cf1b13521338
bigcat0815 <bigcat0815@gamil.com>
2020-08-10 20:49:12 +0900

    #2 ready Project

---
+++ Source/MasterringAcorn/Private/MasterringAcorn.cpp
@@ -0,0 +1,8 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "MasterringAcorn.h"
+#include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(MasterringAcorn);
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MasterringAcorn, "MasterringAcorn" );
+ 
