1c4bb6f51183d965b2ba4cb15a5c88f237d3a3b4
bigcat0815 <bigcat0815@gamil.com>
2020-08-18 16:46:15 +0900

    #7-11 InventoryLogic

--- Source/MasterringAcorn/Private/TESTInventory.cpp
+++ Source/MasterringAcorn/Private/TESTInventory.cpp
@@ -47,15 +47,54 @@ void UTESTInventory::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 
 void UTESTInventory::SelectBestWeapon()
 {
+	int iBestWeaponPower = CurrentWeaponPower;
+	TSubclassOf<class ATESTWeaponBase> bestWeapon = nullptr;
+
+	for (auto WeaponIt = WeaponArray.CreateIterator(); WeaponIt; ++WeaponIt)
+	{
+		FWeaponPropertiesTEST2& currentProps = *WeaponIt;
+		if (currentProps.Ammo == 0) continue;
+		if (currentProps.WeaponPower > iBestWeaponPower)
+		{
+			iBestWeaponPower = currentProps.WeaponPower;
+			bestWeapon = currentProps.WeaponClass;
+			CurrentWeaponPower = iBestWeaponPower;
+		}
+	}
+
+	if (bestWeapon != nullptr)
+	{
+		SelectWeapon(bestWeapon);
+	}
 }
 
 void UTESTInventory::SelectWeapon(TSubclassOf<class ATESTWeaponBase> Weapon)
 {
+	MyOwner->EquipWeaponTEST(Weapon);
+	CurrentWeapon = Weapon;
 }
 
 void UTESTInventory::AddWeapon(TSubclassOf<class ATESTWeaponBase> Weapon, int AmmoCount, uint8 WeaponPower)
 {
+	for (auto WeaponIt = WeaponArray.CreateIterator(); WeaponIt; ++WeaponIt)
+	{
+		FWeaponPropertiesTEST2& currentProps = *WeaponIt;
+		if (currentProps.WeaponClass == CurrentWeapon)
+		{
+			checkSlow(AmmoCount >= 0);
+			currentProps.Ammo += AmmoCount;
+			return;
+		}
+	}
+
+	FWeaponPropertiesTEST2 WeaponProps;
+	WeaponProps.WeaponClass = Weapon;
+	WeaponProps.Ammo = AmmoCount;
+	WeaponProps.WeaponPower = WeaponPower;
+
+	WeaponArray.Add(WeaponProps);
 }
+
 
 void UTESTInventory::AddDefaultWeapon()
 {
@@ -63,18 +102,64 @@ void UTESTInventory::AddDefaultWeapon()
 
 void UTESTInventory::ChangeAmmo(TSubclassOf<class ATESTWeaponBase> Weapon, const int ChangeAmount)
 {
+	for (auto WeaponIt = WeaponArray.CreateIterator(); WeaponIt; ++WeaponIt)
+	{
+		FWeaponPropertiesTEST2& currentProps = *WeaponIt;
+		if (currentProps.WeaponClass == Weapon)
+		{
+			if (currentProps.Ammo == UNLIMITED_AMMO) return;
+
+			currentProps.Ammo = FMath::Clamp(currentProps.Ammo + ChangeAmount, 0, 999);
+
+			if (currentProps.Ammo == 0)
+			{
+				CurrentWeaponPower = -1;
+				SelectBestWeapon();
+			}
+			return;
+		}
+	}
 }
 
 int UTESTInventory::FindCurrentWeaponIndex() const
 {
-	return 0;
+	int iCurrentIndex = 0;
+	for (auto WeaponIt = WeaponArray.CreateConstIterator(); WeaponIt; ++WeaponIt)
+	{
+		const FWeaponPropertiesTEST2& currentProps = *WeaponIt;
+		if (currentProps.WeaponClass == CurrentWeapon) break;
+	}
+	checkSlow(iCurrentIndex < WeaponArray.Num());
+	
+	return iCurrentIndex;
 }
 
 void UTESTInventory::SelectNextWeapon()
 {
+	int iCurrentIndex = FindCurrentWeaponIndex();
+	if (iCurrentIndex == 0) return;
+
+	if (iCurrentIndex == WeaponArray.Num() - 1)
+	{
+		SelectWeapon(WeaponArray[0].WeaponClass);
+	}
+	else
+	{
+		SelectWeapon(WeaponArray[iCurrentIndex + 1].WeaponClass);
+	}
 }
 
 void UTESTInventory::SelectPreviousWeapon()
 {
+	int iCurrentIndex = FindCurrentWeaponIndex();
+	if (iCurrentIndex == 0) return;
+	if (iCurrentIndex > 0)
+	{
+		SelectWeapon(WeaponArray[iCurrentIndex - 1].WeaponClass);
+	}
+	else
+	{
+		SelectWeapon(WeaponArray[WeaponArray.Num() - 1].WeaponClass);
+	}
 }
 
